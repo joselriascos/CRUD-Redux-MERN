64be1562260468107f0347438cdf0fd7edc66e42
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-10-24 13:04:21 -0500

    fixed client bugs

--- Client/src/utils/consts.ts
+++ Client/src/utils/consts.ts
@@ -1,1 +1,1 @@
-export const API_URL: string = 'https://joselriascos-crud-redux-toolkit-api.netlify.app/'
+export const API_URL: string = 'https://joselriascos-crud-redux-toolkit-api.netlify.app/api/users'
